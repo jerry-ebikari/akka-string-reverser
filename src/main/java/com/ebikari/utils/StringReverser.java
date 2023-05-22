5e46489cae0449f4fe301198230ebc7aba4bd7bc
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-05-22 18:14:18 +0100

    Make com.ebikari.utils.StringReverser.reverse() static

--- src/main/java/com/ebikari/utils/StringReverser.java
+++ src/main/java/com/ebikari/utils/StringReverser.java
@@ -2,7 +2,7 @@ package com.ebikari.utils;
 
 public class StringReverser {
 
-    public String reverse(String str) {
+    public static String reverse(String str) {
         StringBuilder sb = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; i--) {
             sb.append(str.charAt(i));
