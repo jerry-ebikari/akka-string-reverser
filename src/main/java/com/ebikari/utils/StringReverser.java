2179975d1e2e16b4fca62847cdbdf58763e484de
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-05-22 18:15:23 +0100

    com.ebikari.utils.StringReverser.reverse: Add null check

--- src/main/java/com/ebikari/utils/StringReverser.java
+++ src/main/java/com/ebikari/utils/StringReverser.java
@@ -3,6 +3,7 @@ package com.ebikari.utils;
 public class StringReverser {
 
     public static String reverse(String str) {
+        if (str == null) throw new IllegalStateException("str may not be null");
         StringBuilder sb = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; i--) {
             sb.append(str.charAt(i));
