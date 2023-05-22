e802ad0ed7f52d882ab1c73e89dbeebe4fd385ac
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-05-22 18:19:02 +0100

    Created com.ebikari.actors.StringReverseActor

---
+++ src/main/java/com/ebikari/actors/StringReverseActor.java
@@ -0,0 +1,10 @@
+package com.ebikari.actors;
+
+import akka.actor.AbstractActor;
+
+public class StringReverseActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
