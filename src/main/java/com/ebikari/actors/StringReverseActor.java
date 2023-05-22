7d54f36c924aa6c03a0e85c62afbb5b528ddc230
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-05-22 18:34:32 +0100

    com.ebikari.actors.StringReverseActor: Implemented createReceive method

--- src/main/java/com/ebikari/actors/StringReverseActor.java
+++ src/main/java/com/ebikari/actors/StringReverseActor.java
@@ -1,10 +1,16 @@
 package com.ebikari.actors;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Status;
+import com.ebikari.utils.StringReverser;
 
 public class StringReverseActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(String.class, message -> sender().tell(StringReverser.reverse(message), ActorRef.noSender()))
+                .matchAny(o -> sender().tell(new Status.Failure(new Exception("Unknown Message")), self()))
+                .build();
     }
 }
